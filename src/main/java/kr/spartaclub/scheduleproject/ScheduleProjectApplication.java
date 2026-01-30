310e00fc2c75d3c4573768e270becc8bacc7c56f
sohyun kim <sohyun050282@gmail.com>
2026-01-30 12:27:38 +0900

    first commit

---
+++ src/main/java/kr/spartaclub/scheduleproject/ScheduleProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.scheduleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectApplication.class, args);
+    }
+
+}
